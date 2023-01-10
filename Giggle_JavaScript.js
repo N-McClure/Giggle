46cc1864907f99eba5d0f26eb6f8909e6b656bcc
N-McClure <64433966+N-McClure@users.noreply.github.com>
2023-01-09 19:08:19 -0500

    Add files via upload

--- Giggle_JavaScript.js
+++ Giggle_JavaScript.js
@@ -1,17 +1,59 @@
-//Function to Validate if the User's Password Matches the Confirmation one in the Log-In page:
+//Functions To Validate Input and Other Fun Things...I Hope:
+
+//Function to Validate if the User's Password Matches the Confirmation one in the Log-In page and then checks to See if it reaches the Minimum Length:
 function passwordMatch()
 {
-    var pw_Set = document.getElementById("PassWord").value;
-    var pw_Match = document.getElementById("ConfirmPassWord").value;
-    
-    let Message;
-    if (pw_Set == pw_Match)
+    //Input from Text Boxes = Variables:
+    let x = document.forms["Fill it Up"]["PassWord"].value;
+    let y = document.forms["Fill it Up"]["ConfirmPassWord"].value;
+
+    //The Comparison:
+    if (x == y)
     {
-        return true;
+        //Nested IF statements to check Length:
+        var length = x.length;
+        //Check the Length:
+        if (length >= 8)
+        {
+            return true;
+        }
+        else if (length < 8)
+        {
+            alert("Password Input does NOT follow the minimum Length.");
+            return false;
+        }
     }
     else
     {
-        window.alert("Passwords DON'T Match.");
+        alert("Password and Confirmation Password Input DON'T Match.");
         return false;
     }
+
 }
+
+//Function to Read and Search Based on the User's Search Query:
+function FindJokes()
+{
+    //Input in the Joke Search Bar = Variables:
+    let x = document.forms["SearchSomething"]["Joke_Search"].value;
+
+    var search = x.toLowerCase();
+
+    //The Searching of Related Links:
+
+    //The Search to Page with the Links Based on Search Query Input:
+    var searchPage = ("https://www.google.com/search?q=" + search + " jokes");
+    var resultPage = ("Joke_Results.html");
+    open(searchPage);
+
+    //The Scraping Part of the Function:
+    //No Idea What I am Doing...Help!
+
+    //Sets the value of variable "hyperLinks" to the data of the HTML <cite> tag:
+    var hyperLinks = searchPage.getElementsByTagName('a');
+    //The Array of Hyper-Links:
+    const Links_Array = [hyperLinks];
+    
+
+}
+
